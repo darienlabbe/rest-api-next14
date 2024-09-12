5a68f2efa196ae994607c01c3bc21972b93697b4
Darien Labbe <darienlabbe@gmail.com>
2024-09-11 23:00:42 -0700

    Added blog search parameters

--- app/api/(dashboard)/blogs/route.ts
+++ app/api/(dashboard)/blogs/route.ts
@@ -9,8 +9,12 @@ export const GET = async (request: Request) => {
     try {
         const { searchParams } = new URL(request.url);
         const userId = searchParams.get("userId");
+        const searchKeywords = searchParams.get("keywords") as string;
+        const startDate = searchParams.get("startDate");
+        const endDate = searchParams.get("endDate");
+        const page: any = parseInt(searchParams.get("page") || "1");
+        const limit: any = parseInt(searchParams.get("limit") || "10");
         
-
         if (!userId ||!Types.ObjectId.isValid(userId)) {
             return new NextResponse(JSON.stringify({ message: "Invalid or missing userId" }), { status: 400 });
         }
@@ -38,9 +42,24 @@ export const GET = async (request: Request) => {
             category: new Types.ObjectId(categoryId),
         };
 
+        if (searchKeywords) {
+            filter.$or = [
+                { title: { $regex: searchKeywords, $options: "i" } }, 
+                { description: { $regex: searchKeywords, $options: "i" } }
+            ];
+        }
 
+        if (startDate && endDate) {
+            filter.createdAt = { $gte: new Date(startDate), $lte: new Date(endDate) };
+        } else if (startDate) {
+            filter.createdAt = { $gte: new Date(startDate) };
+        } else if (endDate) {
+            filter.createdAt = { $lte: new Date(endDate) };
+        }
 
-        const blogs = await Blog.find(filter);
+        const skip = (page - 1) * limit;
+
+        const blogs = await Blog.find(filter).sort({ createdAt: "asc"}).skip(skip).limit(limit);
 
         return new NextResponse(JSON.stringify({ blogs }), { status: 200 });
 
